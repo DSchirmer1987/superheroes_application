ac1b9d2066425eb89f5a48d28bfb5c8f1015b1d7
Daniel Schirmer <d.schirmer1987@gmail.com>
2022-12-22 20:47:45 +0100

    --- Auto Commit Daniel (22.12.2022 - 20:47:45) ---

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,8 +8,29 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  int _selectedIndex = 1;
+  void _onItemTapped(int index) {
+    _selectedIndex = index;
+  }
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Superhero Application"),
+      ),
+      body: Center(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite), label: "Favourite")
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.amber[800],
+      ),
+    );
   }
 }
