9eea7e44237d51feb8a31e9855ffd91deaf7139f
Daniel Schirmer <d.schirmer1987@gmail.com>
2022-12-22 22:40:38 +0100

    --- Auto Commit Daniel (22.12.2022 - 22:40:38) ---

---
+++ lib/model/superhero.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+class Superhero {
+  String? id;
+  String? name;
+  Map<String, String>? powerstats;
+  Map<String, String>? biography;
+  Map<String, String>? appearance;
+  Map<String, String>? work;
+  Map<String, String>? connections;
+  Image? image;
+
+  Superhero(
+      {this.id,
+      this.name,
+      this.powerstats,
+      this.biography,
+      this.appearance,
+      this.work,
+      this.connections,
+      this.image}) {
+    powerstats = <String, String>{};
+  }
+
+  static Superhero fromMap(Map<String, dynamic> map) => Superhero(
+      id: map['id'],
+      name: map['name'],
+      powerstats: map['powerstats'],
+      biography: map['biography'],
+      appearance: map['appearance'],
+      work: map['work'],
+      connections: map['connections'],
+      image: Image.network(map['image']));
+
+  static Superhero fromJson(String data) =>
+      Superhero.fromMap(json.decode(data));
+}
