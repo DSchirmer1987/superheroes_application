acb7157834d7a4c3dd3d91b1f309fa456cb2a3d3
Daniel Schirmer <d.schirmer1987@gmail.com>
2022-12-23 10:27:57 +0100

    --- Auto Commit Daniel (23.12.2022 - 10:27:57) ---

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -13,6 +13,19 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final Widget homebody = ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(items[index]),
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const DetailPage()));
+          },
+        );
+      },
+    );
+
     return Center(
       child: ListView.builder(
         itemCount: items.length,
