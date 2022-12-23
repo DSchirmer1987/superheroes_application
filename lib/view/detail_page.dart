5d542e0c1142799f32c6788cf9b5ac32a9790644
Daniel Schirmer <d.schirmer1987@gmail.com>
2022-12-23 10:53:33 +0100

    --- Auto Commit Daniel (23.12.2022 - 10:53:33) ---

--- lib/view/detail_page.dart
+++ lib/view/detail_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:superheroes_application/model/mockup_data.dart';
 import 'package:superheroes_application/model/superhero.dart';
+import 'package:superheroes_application/view/home_page.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
@@ -11,26 +12,40 @@ class DetailPage extends StatefulWidget {
 
 class _DetailPageState extends State<DetailPage> {
   Superhero superhero = Superhero.fromJson(MockupData.getSuperheroExample());
+  Widget? detailBody;
+
+  @override
+  void initState() {
+    super.initState();
+    detailBody = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          superhero.name,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        Image.network(
+          superhero.imageURL,
+          scale: 2,
+        ),
+        FloatingActionButton(
+          child: const Icon(Icons.arrow_back),
+          onPressed: () {
+            _backToHome;
+          },
+        )
+      ],
+    );
+  }
+
+  void _backToHome() {
+    setState(() {
+      detailBody = const HomePage();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            superhero.name,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-          ),
-          Image.network(
-            superhero.imageURL,
-            scale: 2,
-          ),
-          FloatingActionButton(
-            child: Icon(Icons.arrow_back),
-            onPressed: () {},
-          )
-        ],
-      ),
-    );
+    return Center(child: detailBody);
   }
 }
